ed1584ecfad09a17074e1e8fb5c08e1cfb6470c9
WEMOAP <infowebmobapps@gmail.com>
2017-09-12 10:48:13 +0800

    first commit

---
+++ www/settings.js
@@ -0,0 +1,15 @@
+//For rate and share app
+var android_packageName = "";
+var apple_id = "";
+//URL site wordpress
+var wordpress_url = "";
+//Posts per page
+var wordpress_per_page = 10;
+//For login with Social
+var facebook_app_id = "";
+var google_client_id = "";
+//For push notification with OneSignal
+var onesignal_app_id = "";
+var onesignal_project_number = "";
+//For RTL Language
+var rtl_language = false;
